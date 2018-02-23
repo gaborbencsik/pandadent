9b24236d2d5f7adca1dc19be60e3f7014c2887a0
bencso <gabor.bencsik13@gmail.com>
2018-02-23 14:52:39 +0100

    handle menu on mobile screen, modify services, add images

--- public/index.js
+++ public/index.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
 		anchors:['home', 'services', 'references', 'pricing', 'contact'],
-    sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', '#4BBFC3', '#7BAABE']
+    sectionsColor: ['#4BBFC3', '#7BAABE', '#4BBFC3', '#7BAABE', '#f2f2f2']
+		// menu: '#myMenu'
 	});
 
 	$('.hamburger').click(function() {
+		$('.navbar').slideToggle("fast", function() {});
+	});
+
+	$('.btn.hvr-grow').click(function() {
+		console.log('clicked');
 		$('.navbar').slideToggle("fast", function() {});
 	});
 });
