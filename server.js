5a3f34a3e2ac751fff485f1e0af3194f4973b55d
bencso <gabor.bencsik13@gmail.com>
2018-05-03 17:52:42 +0200

    refresh server.js && add index.html

--- server.js
+++ server.js
@@ -4,39 +4,19 @@ const router = express.Router();
 const serveStatic = require('serve-static');
 const bodyParser = require('body-parser');
 const helmet = require('helmet')
-const gsjson = require('google-spreadsheet-to-json');
-const engines = require('consolidate');
-const _ = require('lodash');
+// const gsjson = require('google-spreadsheet-to-json');
 
 const key = '1p-cbPPmYaDmHrG2D0rz3ryhwQao4N_oUbLumgwrJdS4';
 
 const app = express();
 
+app.use(helmet());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
-app.use(express.static('public/lib'))
-app.use(express.static('public/assets'))
+app.use(serveStatic(__dirname + "/dist"));
 
-app.engine('hbs', engines.handlebars);
-app.set('views', './public');
-app.set('view engine', 'hbs');
-
-app.get('/', (req, res) => {
-  gsjson({
-    spreadsheetId: key,
-    hash: 'field',
-    worksheet: ['Content', 'Images']
-  })
-  .then(function(result) {
-    res.render('index', { data: result[0], images: result[1] }) })
-  .catch(function(err) {
-    console.log(err.message);
-    // res.render('error')
-  });
-})
-
-const server = app.listen(process.env.PORT || 4515, function() {
+const server = app.listen(process.env.PORT || 4516, () => {
   console.log(`Server is running on ${server.address().port}`);
 });
