276fc72aefcaa1ebf411700c68b295e14cc5078b
bencso <gabor.bencsik13@gmail.com>
2018-05-23 16:00:00 +0200

    rebuild site

--- public/index.js
+++ public/index.js
@@ -1,15 +1,163 @@
-$(document).ready(function() {
-	$('#fullpage').fullpage({
-		anchors:['home', 'services', 'references', 'pricing', 'contact'],
-    sectionsColor: ['#4BBFC3', '#7BAABE', '#4BBFC3', '#7BAABE', '#f2f2f2']
-		// menu: '#myMenu'
-	});
+class Controller {
+	constructor() {
+		this.isTimePreferenceChecked = false
 
-	$('.hamburger').click(function() {
-		$('.navbar').slideToggle("fast", function() {});
-	});
+		this.formData = {
+			issue: 'Idöpontkérés',
+			name: null,
+			phone: null,
+			email: null,
+			complaint: null,
+			time_preference: null
+		}
 
-	$('.btn.hvr-grow').click(function() {
-		$('.navbar').slideToggle("fast", function() {});
-	});
-});
+		this.addEvents();
+	}
+
+	prepareFormData() {
+		let inputs = document.querySelectorAll('.form-content');
+
+		let readyForSend = true
+
+		inputs.forEach(input => {
+			if (this.isInputEmpty(input.value)) {
+				this.alertBy(input.name)
+				readyForSend = false
+			}
+
+			if (input.type == 'radio') {
+				if (input.checked) {
+					this.isTimePreferenceChecked = true
+					this.formData[input.name] = input.value
+				}
+			} else {
+				this.formData[input.name] = input.value
+			}
+		});
+
+		let isTimePreferenceChecked = this.checkForTimePreference()
+
+		if (readyForSend && isTimePreferenceChecked) {
+			this.send()
+		}
+	}
+
+	send() {
+		console.log(this.formData);
+		let data = JSON.stringify(this.formData)
+
+		fetch(`/appointment`, {
+        method: 'POST',
+        headers: new Headers({
+					'Content-Type': 'application/json',
+          'X-PANDA': 'OK'
+        }),
+				body: data
+      }).then(response => {
+				this.handleResponse(response)
+      }).catch(error => {
+        console.log('error', error)
+      })
+	}
+
+	handleResponse(response) {
+		if (!(response.status === 200)) {
+			this.showError(null, 'Váratlan hiba történt, kérem próbálja meg újra.')
+		}
+
+		if (response.ok) {
+			this.clearInput()
+			this.showSuccess()
+		}
+	}
+
+	clearInput() {
+		let inputs = document.querySelectorAll('input');
+		let textarea = document.querySelector('textarea');
+		inputs.forEach(input => {
+			input.value = '';
+			input.checked = false;
+		})
+		textarea.value = '';
+	}
+
+	showSuccess() {
+		console.log('success');
+		$('#myModal').modal({show: true})
+	}
+
+	showError(parent, errorMessage) {
+		let parentNode = document.querySelector('header .row');
+		if (parent != null) {
+			parentNode = parent;
+		}
+
+		const error = document.createElement('p');
+		this.createErrorElement(error, errorMessage);
+
+		parentNode.appendChild(error)
+		this.hideError(parentNode, error)
+	}
+
+	createErrorElement(error, errorMessage) {
+		error.innerText = errorMessage;
+		error.classList.add('alert')
+		error.classList.add('alert-danger')
+		error.classList.add('error')
+	}
+
+	hideError(parent, errorElement) {
+		window.setTimeout(() => {
+			parent.removeChild(errorElement);
+		}, 3000)
+	}
+
+	isInputEmpty(inputValue) {
+		return inputValue == null || inputValue == ''
+	}
+
+	alertBy(inputName) {
+		console.log(inputName);
+		if (inputName == 'name') {
+			this.setAlertForInput(inputName, 'Kérem válasszon egy napot!')
+		} else if (inputName == 'phone') {
+			this.setAlertForInput(inputName, 'Kérem adja meg a telefonszámát!')
+		} else if (inputName == 'email') {
+			this.setAlertForInput(inputName, 'Kérem adja meg az email címét!')
+		} else if (inputName == 'complaint') {
+			this.setAlertForInput(inputName, 'Kérem írja meg, hogy mi a panasza!')
+		}
+	}
+
+	setAlertForInput(inputName, message) {
+		let input
+
+		if (inputName == 'complaint') {
+			input = document.querySelector(`textarea[name='${inputName}']`)
+		} else {
+			input = document.querySelector(`input[name='${inputName}']`)
+		}
+
+		let parent = input.parentElement
+		this.showError(parent, message)
+	}
+
+	checkForTimePreference() {
+		if (!this.isTimePreferenceChecked) {
+			let parent = document.querySelector('.time-preference')
+			this.showError(parent, 'Kérem válasszon egy napot!')
+			return false
+		}
+		return true
+	}
+
+
+	addEvents() {
+		let sendButton = document.querySelector('#send');
+		sendButton.addEventListener('click', () => {
+			this.prepareFormData();
+		});
+	}
+}
+
+const controller = new Controller();
--- server.js
+++ server.js
@@ -7,11 +7,15 @@ const helmet = require('helmet')
 const gsjson = require('google-spreadsheet-to-json');
 const engines = require('consolidate');
 const _ = require('lodash');
+const emailValidator = require('email-validator');
+const dotenv = require('dotenv').config();
+const fetch = require('node-fetch');
 
 const key = '1p-cbPPmYaDmHrG2D0rz3ryhwQao4N_oUbLumgwrJdS4';
-const key2 = '1xHYqR8dt1ip3l0qJxyVcXJ9znKHZJ32h3ndYoQ67y2I';
 
 const app = express();
+
+app.use(helmet());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,18 +29,40 @@ app.set('views', './public');
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-  gsjson({
-    spreadsheetId: key,
-    hash: 'field',
-    worksheet: ['Content', 'Images']
-  })
-  .then(function(result) {
-    res.render('index', {data: result[0], images: result[1]});
-  })
-  .catch(function(err) {
-    console.log(err.message);
-    // res.render('error')
-  });
+  // gsjson({
+  //   spreadsheetId: key,
+  //   hash: 'field',
+  //   worksheet: ['Content', 'Images']
+  // })
+  // .then(function(result) {
+  //   res.render('index', { data: result[0], images: result[1] }) })
+  // .catch(function(err) {
+  //   console.log(err.message);
+  //   // res.render('error')
+  // });
+
+  let data = {
+    title: 'Dr. Kovács Anna gyermek- és felnőttfogorvos',
+    makeAppointment: 'Idöpont kérés'
+  };
+
+  res.render('index', {data})
+})
+
+app.post('/appointment', (req, res) => {
+  let isHeaderExist = req.headers['x-panda'] == 'OK'
+
+  if (!isHeaderExist) {
+    res.status(401).send({success: false})
+  } else {
+    let body = JSON.stringify(req.body)
+
+    fetch(process.env.ZAPIER_URL, { method: 'POST', body: body, headers: { 'Content-Type': 'application/json', 'X-Recurse-Parse': false }, })
+      .then(res => res.json())
+      .then(json => console.log('json', json));
+
+    res.send({success: true})
+  }
 })
 
 const server = app.listen(process.env.PORT || 4515, function() {
